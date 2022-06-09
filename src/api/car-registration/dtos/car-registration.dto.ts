cd226d3e9d354d6c66c30729c68ef42bdc026647
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-09 18:02:04 -0300

    feat: create car-registration dto

---
+++ src/api/car-registration/dtos/car-registration.dto.ts
@@ -0,0 +1,28 @@
+import { IsNotEmpty, IsString } from "class-validator";
+export class CreateCarRegistrationDto {
+
+    @IsString()
+    @IsNotEmpty()
+    carId: string;
+
+    @IsString()
+    @IsNotEmpty()
+    dailyPrice: string;
+
+    @IsString()
+    @IsNotEmpty()
+    brand: string;
+
+    @IsString()
+    @IsNotEmpty()
+    model: string;
+
+    @IsString()
+    @IsNotEmpty()
+    year: string;
+
+    @IsString()
+    @IsNotEmpty()
+    category: string;
+
+}
