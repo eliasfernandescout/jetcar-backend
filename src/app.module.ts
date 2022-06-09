f53f23528080dda637da1b161f017ef52f4e32ef
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-09 18:01:21 -0300

    chore: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
+import { CarRegistrationModule } from './api/car-registration/car-registration.module';
 
 @Module({
-  imports: [],
+  imports: [CarRegistrationModule
+    //PASSAR INIT MONGO
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
