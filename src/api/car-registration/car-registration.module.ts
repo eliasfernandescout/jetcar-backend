34f95800b6ed6d708cb229cfcc1e3d4a20b8118d
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-10 13:03:43 -0300

    chore: update car registration module

--- src/api/car-registration/car-registration.module.ts
+++ src/api/car-registration/car-registration.module.ts
@@ -1,11 +1,12 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { CarRegistrationController } from './controllers/car-registration.controller';
 import { CarRegistrationService } from './services/car-registration.service';
-
+import { CarsSchema } from './model/car-registration.model';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'Car', schema: CarsSchema }])],
   providers: [CarRegistrationService],
   controllers: [CarRegistrationController],
 })
-export class CarRegistrationModule { }
+export class CarRegistrationModule {}
