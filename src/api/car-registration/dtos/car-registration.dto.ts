0c8d273739f65bf42814f9a7914ef1bd8e3ab3c1
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-14 22:13:34 -0300

    refactor: change daily price to number

--- src/api/car-registration/dtos/car-registration.dto.ts
+++ src/api/car-registration/dtos/car-registration.dto.ts
@@ -1,12 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class CreateCarRegistrationDto {
   @IsString()
   @IsNotEmpty()
   licensePlate: string;
 
-  @IsString()
+  @IsNumber()
   @IsNotEmpty()
-  dailyPrice: string;
+  dailyPrice: number;
 
   @IsString()
   @IsNotEmpty()
