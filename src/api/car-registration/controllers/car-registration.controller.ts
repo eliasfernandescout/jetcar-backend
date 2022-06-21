7148d98dc219aecd399338a0c723d40e3bc6608d
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-20 23:25:21 -0300

    add file uploader

--- src/api/car-registration/controllers/car-registration.controller.ts
+++ src/api/car-registration/controllers/car-registration.controller.ts
@@ -6,9 +6,14 @@ import {
   HttpStatus,
   Post,
   Res,
+  UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, MulterModule } from '@nestjs/platform-express';
+import e from 'express';
+import { diskStorage } from 'multer';
+// import * as path from 'path';
+import path from 'path';
 import { Car } from '../model/car-registration.model';
 import { CarRegistrationService } from '../services/car-registration.service';
 
@@ -38,11 +43,73 @@ export class CarRegistrationController {
     return this.carRegistrationService.getByCategorySUV();
   }
 
-  @Post('car-register')
-  @UseInterceptors(FileInterceptor('file'))
-  @HttpCode(HttpStatus.CREATED)
-  register(@Body() car: Car) {
-    // response.status(204).send('Registro criado com sucesso!');
-    return this.carRegistrationService.registerCar(car);
+  // @Post('car-register')
+  // @UseInterceptors(FileInterceptor('file'))
+  // @UseInterceptors(
+  //   FileInterceptor('image', {
+  //     storage: diskStorage({
+  //       destination: './src/api/car-imgs',
+  //       filename: (req, file, callBack) => {
+  //         const fileName =
+  //           path.parse(file.originalname).name.replace(/\s/g, '') + Date.now();
+  //         const extension = path.parse(file.originalname).ext;
+  //         callBack(null, `${fileName}${extension}`);
+  //       },
+  //     }),
+  //   }),
+  // )
+  // @HttpCode(HttpStatus.CREATED)
+  // register(@Body() car: Car, @Res() res, @UploadedFile() file) {
+  //   const fileUploadSuccess = res.status(HttpStatus.OK).json({
+  //     success: true,
+  //     data: file.path,
+  //   });
+  //   const carRegistrationSuccess = this.carRegistrationService.registerCar(car);
+  //   return { fileUploadSuccess, carRegistrationSuccess };
+  // }
+
+  // @Post('upload-img')
+  // @UseInterceptors(
+  //   FileInterceptor('image', {
+  //     storage: diskStorage({
+  //       destination: './src/api/carImgs',
+  //       filename: (req, file, callBack) => {
+  //         const fileName =
+  //           path.parse(file.originalname).name.replace(/\s/g, '') + Date.now();
+  //         const extension = path.parse(file.originalname).ext;
+  //         callBack(null, `${fileName}${extension}`);
+  //       },
+  //     }),
+  //   }),
+  // )
+  // uploadImg(@Res() res, @UploadedFile() file) {
+  //   return res.status(HttpStatus.OK).json({
+  //     success: true,
+  //     data: file.path,
+  //   });
+  // }
+
+  @Post('upload-img')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './src/api/carImgs',
+        filename(
+          req: e.Request,
+          file: Express.Multer.File,
+          callBack: (error: Error | null, filename: string) => void,
+        ) {
+          const name = file.originalname.split('.')[0];
+          const fileExtension = file.originalname.split('.')[1];
+          const newFileName =
+            name.split('.').join('_') + '_' + Date.now() + '_' + fileExtension;
+
+          callBack(null, newFileName);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
